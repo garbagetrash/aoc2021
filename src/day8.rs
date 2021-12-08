d0670e0e7f2240a0920db34a81180199fdceda49
Kyle Harris <kaharrs2@gmail.com>
2021-12-08 10:36:19 -0700

    Added Rayon, helps sometimes.

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use rayon::prelude::*;
 
 #[allow(clippy::comparison_to_empty)]
 #[aoc_generator(day8)]
@@ -39,118 +40,118 @@ pub fn part1(input: &[(Vec<String>, Vec<String>)]) -> i64 {
     output
 }
 
+pub fn do_word(s: &(Vec<String>, Vec<String>)) -> i64 {
+    let mut dict: HashMap<i64, HashSet<char>> = HashMap::new();
+    let mut keys = vec![];
+    for word in &s.0 {
+        let mut key: HashSet<char> = HashSet::new();
+        for c in word.chars() {
+            key.insert(c);
+        }
+        keys.push(key);
+    }
+
+    // First ID the unique guys
+    for key in &keys {
+        let wlen = key.len();
+        if wlen == 2 {
+            // 1 found
+            dict.insert(1, key.clone());
+        } else if wlen == 3 {
+            // 7 found
+            dict.insert(7, key.clone());
+        } else if wlen == 4 {
+            // 4 found
+            dict.insert(4, key.clone());
+        } else if wlen == 7 {
+            // 8 found
+            dict.insert(8, key.clone());
+        }
+    }
+
+    // 5 doesn't contain 4
+    // 9 contains 4!!!
+    // 2 doesn't contain 4
+    // 3 doesn't contain 4
+    // 6 doesn't contain 4
+    //
+    for key in &keys {
+        if key.len() == 2 || key.len() == 3 || key.len() == 4 || key.len() == 7 {
+            // Already have it
+        } else {
+            // Solve 9
+            let intersection: HashSet<char> =
+                key.intersection(dict.get(&4).unwrap()).copied().collect();
+            let set4: &HashSet<char> = dict.get(&4).unwrap();
+            if key.len() == 6 && &intersection == set4 {
+                dict.insert(9, key.clone());
+                continue;
+            }
+
+            // Solve 0
+            let intersection: HashSet<char> =
+                key.intersection(dict.get(&1).unwrap()).copied().collect();
+            let set1: &HashSet<char> = dict.get(&1).unwrap();
+            if key.len() == 6 && &intersection == set1 {
+                dict.insert(0, key.clone());
+                continue;
+            }
+
+            // Solve 6
+            if key.len() == 6 {
+                dict.insert(6, key.clone());
+                continue;
+            }
+
+            // Need 2, 3, 5
+        }
+    }
+
+    for key in keys {
+        if key.len() == 5 {
+            // Solve 3
+            let intersection: HashSet<char> =
+                key.intersection(dict.get(&1).unwrap()).copied().collect();
+            let set1: &HashSet<char> = dict.get(&1).unwrap();
+            if key.len() == 5 && &intersection == set1 {
+                dict.insert(3, key.clone());
+                continue;
+            }
+
+            // Solve 5
+            let intersection: HashSet<char> =
+                key.intersection(dict.get(&9).unwrap()).copied().collect();
+            let set5: &HashSet<char> = &key;
+            if key.len() == 5 && &intersection == set5 {
+                dict.insert(5, key.clone());
+                continue;
+            }
+
+            // Else 2
+            dict.insert(2, key.clone());
+        }
+    }
+
+    // SOLVE
+    let mut tempvalue: i64 = 0;
+    for word in &s.1 {
+        let setword = word.chars().collect::<HashSet<_>>();
+
+        for (k, v) in dict.iter() {
+            if *v == setword {
+                let digit = k;
+                tempvalue *= 10;
+                tempvalue += digit;
+                break;
+            }
+        }
+    }
+    tempvalue
+}
+
 #[aoc(day8, part2)]
 pub fn part2(input: &[(Vec<String>, Vec<String>)]) -> i64 {
-    let mut output = 0;
-    for s in input {
-        let mut dict: HashMap<i64, HashSet<char>> = HashMap::new();
-        let mut keys = vec![];
-        for word in &s.0 {
-            let mut key: HashSet<char> = HashSet::new();
-            for c in word.chars() {
-                key.insert(c);
-            }
-            keys.push(key);
-        }
-
-        // First ID the unique guys
-        for key in &keys {
-            let wlen = key.len();
-            if wlen == 2 {
-                // 1 found
-                dict.insert(1, key.clone());
-            } else if wlen == 3 {
-                // 7 found
-                dict.insert(7, key.clone());
-            } else if wlen == 4 {
-                // 4 found
-                dict.insert(4, key.clone());
-            } else if wlen == 7 {
-                // 8 found
-                dict.insert(8, key.clone());
-            }
-        }
-
-        // 5 doesn't contain 4
-        // 9 contains 4!!!
-        // 2 doesn't contain 4
-        // 3 doesn't contain 4
-        // 6 doesn't contain 4
-        //
-        for key in &keys {
-            if key.len() == 2 || key.len() == 3 || key.len() == 4 || key.len() == 7 {
-                // Already have it
-            } else {
-                // Solve 9
-                let intersection: HashSet<char> =
-                    key.intersection(dict.get(&4).unwrap()).copied().collect();
-                let set4: &HashSet<char> = dict.get(&4).unwrap();
-                if key.len() == 6 && &intersection == set4 {
-                    dict.insert(9, key.clone());
-                    continue;
-                }
-
-                // Solve 0
-                let intersection: HashSet<char> =
-                    key.intersection(dict.get(&1).unwrap()).copied().collect();
-                let set1: &HashSet<char> = dict.get(&1).unwrap();
-                if key.len() == 6 && &intersection == set1 {
-                    dict.insert(0, key.clone());
-                    continue;
-                }
-
-                // Solve 6
-                if key.len() == 6 {
-                    dict.insert(6, key.clone());
-                    continue;
-                }
-
-                // Need 2, 3, 5
-            }
-        }
-
-        for key in keys {
-            if key.len() == 5 {
-                // Solve 3
-                let intersection: HashSet<char> =
-                    key.intersection(dict.get(&1).unwrap()).copied().collect();
-                let set1: &HashSet<char> = dict.get(&1).unwrap();
-                if key.len() == 5 && &intersection == set1 {
-                    dict.insert(3, key.clone());
-                    continue;
-                }
-
-                // Solve 5
-                let intersection: HashSet<char> =
-                    key.intersection(dict.get(&9).unwrap()).copied().collect();
-                let set5: &HashSet<char> = &key;
-                if key.len() == 5 && &intersection == set5 {
-                    dict.insert(5, key.clone());
-                    continue;
-                }
-
-                // Else 2
-                dict.insert(2, key.clone());
-            }
-        }
-
-        // SOLVE
-        let mut tempvalue: i64 = 0;
-        for word in &s.1 {
-            let setword = word.chars().collect::<HashSet<_>>();
-
-            for (k, v) in dict.iter() {
-                if *v == setword {
-                    let digit = k;
-                    tempvalue *= 10;
-                    tempvalue += digit;
-                    break;
-                }
-            }
-        }
-        output += tempvalue;
-    }
-    output
+    input.par_iter().map(|s| do_word(s)).sum()
 }
 
 #[cfg(test)]
