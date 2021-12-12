89ddd2534b7c20226b733d5cdb5c947569cecde2
Kyle Harris <kaharrs2@gmail.com>
2021-12-12 18:26:26 -0500

    Day 12

---
+++ src/day11.rs
@@ -0,0 +1,186 @@
+use rayon::prelude::*;
+
+struct Board {
+    counter: Vec<Vec<u32>>,
+    flash: [[bool; 10]; 10],
+}
+
+impl Board {
+    pub fn new(counter: &Vec<Vec<u32>>) -> Board {
+        Board {
+            counter: counter.clone(),
+            flash: [[false; 10]; 10],
+        }
+    }
+
+    pub fn increment(&mut self) {
+        for row in &mut self.counter {
+            for col in row {
+                *col += 1;
+            }
+        }
+    }
+
+    pub fn print_board(&self) {
+        for row in &self.counter {
+            for col in row {
+                print!("{} ", col);
+            }
+            println!("");
+        }
+    }
+
+    pub fn flash_point(&mut self, point: (usize, usize)) -> usize {
+        let x = point.0;
+        let y = point.1;
+        let mut ncnt = 0;
+        if !self.flash[x][y] && self.counter[x][y] > 9 {
+            self.flash[x][y] = true;
+            ncnt += 1;
+            // Increment neighbors
+            let neighbors = get_neighbors((x, y));
+            for neighbor in neighbors {
+                let nx = neighbor.0;
+                let ny = neighbor.1;
+                if !self.flash[nx][ny] {
+                    self.counter[nx][ny] += 1;
+                }
+                ncnt += self.flash_point(neighbor);
+            }
+        }
+        ncnt
+    }
+
+    pub fn flash(&mut self) -> usize {
+        let mut ncnt = 0;
+        let mut last_ncnt = ncnt;
+        loop {
+            // Check board
+            for x in 0..10 {
+                for y in 0..10 {
+                    ncnt += self.flash_point((x, y));
+                }
+            }
+
+            if ncnt == last_ncnt {
+                break;
+            }
+            last_ncnt = ncnt;
+        }
+
+        // Reset state
+        self.flash = [[false; 10]; 10];
+        for x in 0..10 {
+            for y in 0..10 {
+                if self.counter[x][y] >= 10 {
+                    self.counter[x][y] = 0;
+                }
+            }
+        }
+        ncnt
+    }
+
+    pub fn check_sync(&self) -> bool {
+        for x in 0..10 {
+            for y in 0..10 {
+                if self.counter[x][y] != 0 {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[aoc_generator(day11)]
+pub fn load_input(input: &str) -> Vec<Vec<u32>> {
+    let mut output = vec![];
+    for line in input.lines() {
+        let mut out_line = vec![];
+        for c in line.chars() {
+            out_line.push(c.to_digit(10).unwrap());
+        }
+        output.push(out_line);
+    }
+    output
+}
+
+fn get_neighbors(point: (usize, usize)) -> Vec<(usize, usize)> {
+    let mut points = vec![];
+    if point.0 > 0 {
+        points.push((point.0 - 1, point.1));
+        if point.1 > 0 {
+            points.push((point.0 - 1, point.1 - 1));
+        }
+        if point.1 < 9 {
+            points.push((point.0 - 1, point.1 + 1));
+        }
+    }
+    if point.0 < 9 {
+        points.push((point.0 + 1, point.1));
+        if point.1 > 0 {
+            points.push((point.0 + 1, point.1 - 1));
+        }
+        if point.1 < 9 {
+            points.push((point.0 + 1, point.1 + 1));
+        }
+    }
+    if point.1 > 0 {
+        points.push((point.0, point.1 - 1));
+    }
+    if point.1 < 9 {
+        points.push((point.0, point.1 + 1));
+    }
+
+    points
+}
+
+#[aoc(day11, part1)]
+pub fn part1(input: &Vec<Vec<u32>>) -> usize {
+    let mut score = 0;
+    let mut board = Board::new(input);
+
+    for i in 0..100 {
+        //board.print_board();
+        board.increment();
+        //board.print_board();
+        let ncnt = board.flash();
+        score += ncnt;
+    }
+    score
+}
+
+#[aoc(day11, part2)]
+pub fn part2(input: &Vec<Vec<u32>>) -> usize {
+    let mut board = Board::new(input);
+    let mut idx = 0;
+    loop {
+        idx += 1;
+        board.increment();
+        board.flash();
+        if board.check_sync() {
+            break;
+        }
+    }
+    idx
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_part1() {
+        let input = read_to_string("input/2021/11.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part1(&input), 1656);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = read_to_string("input/2021/11.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part2(&input), 195);
+    }
+}
---
+++ src/day12.rs
@@ -0,0 +1,211 @@
+use rayon::prelude::*;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum Size {
+    Small,
+    Large,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Edge {
+    pub node0: String,
+    pub node1: String,
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Cave {
+    pub name: String,
+    pub size: Size,
+}
+
+impl Cave {
+    pub fn new(name: &str) -> Cave {
+        let mut size = Size::Small;
+        if name.to_uppercase() == name {
+            size = Size::Large;
+        }
+        Cave {
+            name: String::from(name),
+            size: size,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct CaveSystem {
+    pub caves: HashSet<Cave>,
+    pub edges: HashSet<Edge>,
+    pub count_map: HashMap<String, usize>,
+}
+
+impl CaveSystem {
+    pub fn new() -> CaveSystem {
+        CaveSystem {
+            caves: HashSet::new(),
+            edges: HashSet::new(),
+            count_map: HashMap::new(),
+        }
+    }
+
+    pub fn neighbors(&self, cave: &str) -> Vec<String> {
+        let mut output = vec![];
+        for e in &self.edges {
+            if e.node0 == cave || e.node1 == cave {
+                if e.node0 != cave {
+                    output.push(e.node0.clone());
+                } else {
+                    output.push(e.node1.clone());
+                }
+            }
+        }
+        output
+    }
+
+    pub fn traverse(&self, path: Vec<String>) -> Vec<Vec<String>> {
+        let start_conns = self.neighbors(&path[path.len() - 1]);
+        let valid_choices: Vec<String> = start_conns
+            .iter()
+            .filter(|c| can_visit(&path, &c))
+            .map(|x| (*x).clone())
+            .collect();
+        let mut paths = vec![path.clone()];
+        for conn in valid_choices {
+            let mut new_path = path.clone();
+            new_path.push(conn.clone());
+            if conn == "end" {
+                paths.push(new_path);
+            } else {
+                for rpath in self.traverse(new_path) {
+                    paths.push(rpath);
+                }
+            }
+        }
+        paths
+    }
+
+    pub fn traverse2(&self, path: Vec<String>) -> Vec<Vec<String>> {
+        let start_conns = self.neighbors(&path[path.len() - 1]);
+        let valid_choices: Vec<String> = start_conns
+            .iter()
+            .filter(|c| can_visit2(&path, &c))
+            .map(|x| (*x).clone())
+            .collect();
+        let mut paths = vec![path.clone()];
+        for conn in valid_choices {
+            let mut new_path = path.clone();
+            new_path.push(conn.clone());
+            if conn == "end" {
+                paths.push(new_path);
+            } else {
+                for rpath in self.traverse2(new_path) {
+                    paths.push(rpath);
+                }
+            }
+        }
+        paths
+    }
+}
+
+pub fn visited_any_twice(path: &Vec<String>) -> bool {
+    for (i , c) in path.iter().enumerate() {
+        for (j, other) in path.iter().enumerate() {
+            if c == other && i != j && Cave::new(c).size == Size::Small {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+pub fn can_visit(path: &Vec<String>, cave: &str) -> bool {
+    for c in path {
+        if cave == c && Cave::new(cave).size == Size::Small {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn can_visit2(path: &Vec<String>, cave: &str) -> bool {
+    if visited_any_twice(path) {
+        for c in path {
+            if cave == c && Cave::new(cave).size == Size::Small {
+                return false;
+            }
+        }
+    }
+    if cave == "start" {
+        return false;
+    }
+    true
+}
+
+#[aoc_generator(day12)]
+pub fn load_input(input: &str) -> CaveSystem {
+    let mut cave_system = CaveSystem::new();
+    for line in input.lines() {
+        let mut linegen = line.split('-');
+        let left = linegen.next().unwrap();
+        let right = linegen.next().unwrap();
+        cave_system.caves.insert(Cave::new(left));
+        cave_system.caves.insert(Cave::new(right));
+        cave_system.edges.insert(Edge {
+            node0: String::from(left),
+            node1: String::from(right),
+        });
+    }
+    cave_system
+}
+
+#[aoc(day12, part1)]
+pub fn part1(input: &CaveSystem) -> usize {
+    let cs = input.clone();
+    let mut paths = cs.traverse(vec!["start".to_string()]);
+    paths = paths.iter().filter(|p| p[p.len() - 1] == "end").map(|p| (*p).clone()).collect();
+    paths.len()
+}
+
+#[aoc(day12, part2)]
+pub fn part2(input: &CaveSystem) -> usize {
+    let cs = input.clone();
+    let mut paths = cs.traverse2(vec!["start".to_string()]);
+    paths = paths.iter().filter(|p| p[p.len() - 1] == "end").map(|p| (*p).clone()).collect();
+    paths.len()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs::read_to_string;
+
+    #[test]
+    fn test_part1() {
+        let input = read_to_string("input/2021/12a.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part1(&input), 10);
+
+        let input = read_to_string("input/2021/12b.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part1(&input), 19);
+
+        let input = read_to_string("input/2021/12c.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part1(&input), 226);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = read_to_string("input/2021/12a.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part2(&input), 36);
+
+        let input = read_to_string("input/2021/12b.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part2(&input), 103);
+
+        let input = read_to_string("input/2021/12c.txt").unwrap();
+        let input = load_input(&input);
+        assert_eq!(part2(&input), 3509);
+    }
+}
